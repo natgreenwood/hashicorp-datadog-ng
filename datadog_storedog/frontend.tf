9c71840f404a9a8612c1582ef941ecd6079e9405
aronday <aron.day@datadoghq.com>
2023-04-17 15:37:18 +0100

    Storedog Frontend Update

--- datadog_storedog/frontend.tf
+++ datadog_storedog/frontend.tf
@@ -1,105 +1,112 @@
-resource "kubernetes_manifest" "deployment_frontend" {
-  manifest = {
-    "apiVersion" = "apps/v1"
-    "kind"       = "Deployment"
-    "metadata" = {
-      "labels" = {
-        "app"                    = "ecommerce"
-        "service"                = "frontend"
-        "tags.datadoghq.com/env" = "development"
-      }
-      "name" = "frontend"
-      "namespace" = "storedog"
+resource "kubernetes_deployment" "deploy_frontend" {
+  metadata {
+    labels = {
+      "app"                    = "ecommerce"
+      "service"                = "frontend"
+      "tags.datadoghq.com/env" = "development"
     }
-    "spec" = {
-      "replicas" = 1
-      "selector" = {
-        "matchLabels" = {
-          "app"     = "ecommerce"
-          "service" = "frontend"
+    name      = "frontend"
+    namespace = "default"
+  }
+  spec {
+    replicas = 4
+
+    selector {
+      match_labels = {
+        app     = "ecommerce"
+        service = "frontend"
+      }
+    }
+    strategy {
+      rolling_update {
+        max_surge       = "25%"
+        max_unavailable = "25%"
+      }
+      type = "RollingUpdate"
+    }
+
+    template {
+      metadata {
+        labels = {
+          "app"                    = "ecommerce"
+          "service"                = "frontend"
+          "tags.datadoghq.com/env" = "development"
         }
       }
-      "strategy" = {
-        "rollingUpdate" = {
-          "maxSurge"       = "25%"
-          "maxUnavailable" = "25%"
-        }
-        "type" = "RollingUpdate"
-      }
-      "template" = {
-        "metadata" = {
-          "labels" = {
-            "app"                    = "ecommerce"
-            "service"                = "frontend"
-            "tags.datadoghq.com/env" = "development"
+
+      spec {
+        container {
+          args    = ["docker-entrypoint.sh"]
+          command = ["sh"]
+          env {
+            name  = "DB_USERNAME"
+            value = "user"
           }
-        }
-        "spec" = {
-          "containers" = [
-            {
-              "args" = [
-                "docker-entrypoint.sh",
-              ]
-              "command" = [
-                "sh",
-              ]
-              "env" = [
-                {
-                  "name"  = "DB_USERNAME"
-                  "value" = "user"
-                },
-                {
-                  "name" = "DB_PASSWORD"
-                  "valueFrom" = {
-                    "secretKeyRef" = {
-                      "key"  = "pw"
-                      "name" = "db-password"
-                    }
-                  }
-                },
-                {
-                  "name" = "DD_AGENT_HOST"
-                  "valueFrom" = {
-                    "fieldRef" = {
-                      "fieldPath" = "status.hostIP"
-                    }
-                  }
-                },
-                {
-                  "name"  = "DD_LOGS_INJECTION"
-                  "value" = "true"
-                },
-                {
-                  "name" = "DD_ENV"
-                  "valueFrom" = {
-                    "fieldRef" = {
-                      "fieldPath" = "metadata.labels['tags.datadoghq.com/env']"
-                    }
-                  }
-                },
-                {
-                  "name"  = "DD_ANALYTICS_ENABLED"
-                  "value" = "true"
-                },
-              ]
-              "image"           = "ddtraining/storefront:latest"
-              "imagePullPolicy" = "Always"
-              "name"            = "ecommerce-spree-observability"
-              "ports" = [
-                {
-                  "containerPort" = 3000
-                  "protocol"      = "TCP"
-                },
-              ]
-              "resources" = {
-                "limits" = {}
-                "requests" = {
-                  "cpu"    = "100m"
-                  "memory" = "100Mi"
-                }
+
+          env {
+            name = "DB_PASSWORD"
+            value_from {
+              secret_key_ref {
+                name = "db-password"
+                key  = "pw"
               }
-            },
-          ]
+            }
+          }
+          env {
+            name = "DD_AGENT_HOST"
+            value_from {
+              field_ref {
+                field_path = "status.hostIP"
+              }
+            }
+          }
+
+          env {
+            name  = "DD_LOGS_INJECTION"
+            value = true
+          }
+
+          env {
+            name = "DD_ENV"
+            value_from {
+              field_ref {
+                field_path = "metadata.labels['tags.datadoghq.com/env']"
+              }
+            }
+          }
+
+          env {
+            name  = "DD_ANALYTICS_ENABLED"
+            value = true
+          }
+
+          env {
+            name  = "DD_CLIENT_TOKEN"
+            value = var.DD_CLIENT_TOKEN
+          }
+
+          env {
+            name  = "DD_APPLICATION_ID"
+            value = var.DD_APPLICATION_ID
+          }
+
+
+          image             = "ddtraining/storefront:latest"
+          image_pull_policy = "Always"
+          name              = "ecommerce-spree-observability"
+          port {
+            container_port = 3000
+            protocol       = "TCP"
+          }
+
+          resources {
+            # "limits" = {}
+            requests = {
+              cpu    = "100m"
+              memory = "100Mi"
+            }
+          }
+
         }
       }
     }
